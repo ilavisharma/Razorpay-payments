d036f4dbae6669cebadabebd56d4a5aca1199bb9
ilavisharma <lavisharma796@gmail.com>
2019-05-02 10:50:14 +0530

    front end form setup complete

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 
 const app = express();
 app.use(express.json());
 
+// Handlebars middleware
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Static folder
+app.use(express.static(`${__dirname}/public`));
+
+// Index Route
 app.get('/', (req, res) => {
-  res.send('Hello');
+  res.render('index', {
+    key_id: process.env.KEY_ID
+  });
+});
+
+app.post('/purchase', (req, res) => {
+  console.log(req.body);
+  res.send('Success');
 });
 
 const port = process.env.PORT || 5000;
